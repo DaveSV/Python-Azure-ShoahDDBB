578e2147d7024b5d8b75113b9940d8ed5ff91209
Alberto Saenz <alb.saenz@gmail.com>
2022-07-15 11:51:31 -0600

    Add files via upload
    corrigiendo static urls

--- project/settings.py
+++ project/settings.py
@@ -130,8 +130,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
