6318c1e594cb3dbff19cc811315afacfd966e274
DaveSV <alb.saenz@gmail.com>
2022-07-22 13:23:35 -0600

    app shoah database

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dog_shelters.urls')),
+    path('', include('shoahDDBB.urls')),
 ]
