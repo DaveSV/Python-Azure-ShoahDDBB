2c569bdbab4ca7715572fee329a1fd71175add4e
DaveSV <alb.saenz@gmail.com>
2022-08-01 17:58:50 -0600

    primer upload

--- dog_shelters/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Shelter, Dog
-
-admin.site.register(Shelter)
-admin.site.register(Dog)
--- shoahDDBB/models.py
+++ shoahDDBB/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Movies(models.Model):
+   
+    rating = (
+        ("G", 'G'),
+        ("PG", 'PG'),
+        ("PG-13", 'PG-13'),
+        ("R", 'R'),
+        ("NC-17", "NC-17"),
+        ("Unrated", "Unrated")
+    )
+
+    mov_id = models.ForeignKey
+    mov_name = models.CharField(max_length=100)
+    mov_year = models.CharField(max_length=4)
+    mov_country = models.CharField(max_length=30, null=True)
+    mov_director = models.CharField(max_length=100, null=True)
+    mov_language = models.CharField(max_length=20, null=True)
+    mov_overview = models.TextField()
+    url_movie = models.CharField(max_length=100, null=True)
+    url_trailer = models.CharField(max_length=100, null=True)
+    mov_poster = models.CharField(max_length=100, null=True)
+    mov_streaming = models.CharField(max_length=100, null=True)
+    mov_rating = models.CharField(max_length=10, choices=rating, default="PG", null=True)
+    
+    def __str__(self):
+        return self.mov_name
+
+    
