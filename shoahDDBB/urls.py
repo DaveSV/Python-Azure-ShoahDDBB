0484e2fc5e470704405010e87f103ad611d7c497
DaveSV <alb.saenz@gmail.com>
2022-08-12 20:47:12 -0600

    rutas

--- shoahDDBB/urls.py
+++ shoahDDBB/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
-from shoahDDBB.views import catalog
+from shoahDDBB.views import catalog, book
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('catalog/', views.catalog.as_view(), name='catalog'),
+    path('book/', views.book, name='book'),
+    path('user/', views.user, name='user'),
+    path('search/', views.search, name='search'),
+    path('author/', views.author, name='author'),
 ]
