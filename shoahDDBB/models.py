a8a9faa6f653927ea5b23fd89739769790fb132e
DaveSV <alb.saenz@gmail.com>
2022-08-17 12:20:17 -0600

    actualización de base de datos

--- shoahDDBB/models.py
+++ shoahDDBB/models.py
@@ -15,15 +15,15 @@ class Movies(models.Model):
     mov_id = models.ForeignKey
     mov_name = models.CharField(max_length=100)
     mov_year = models.CharField(max_length=4)
-    mov_country = models.CharField(max_length=30, null=True)
-    mov_director = models.CharField(max_length=100, null=True)
-    mov_language = models.CharField(max_length=20, null=True)
+    mov_country = models.CharField(max_length=30)
+    mov_director = models.CharField(max_length=100)
+    mov_language = models.CharField(max_length=20)
     mov_overview = models.TextField()
-    url_movie = models.CharField(max_length=100, null=True)
-    url_trailer = models.CharField(max_length=100, null=True)
-    mov_poster = models.CharField(max_length=100, null=True)
-    mov_streaming = models.CharField(max_length=100, null=True)
-    mov_rating = models.CharField(max_length=10, choices=rating, default="PG", null=True)
+    url_movie = models.CharField(max_length=100, blank=True)
+    url_trailer = models.CharField(max_length=100, blank=True)
+    mov_poster = models.CharField(max_length=100, blank=True)
+    mov_streaming = models.CharField(max_length=100, blank=True)
+    mov_rating = models.CharField(max_length=10, choices=rating, default="PG")
     
     def __str__(self):
         return self.mov_name
